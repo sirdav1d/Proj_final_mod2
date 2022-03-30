61be6e7607944e16089a84c55dd66e25f3df2db7
sirdav1d <ddavid.diniz@gmail.com>
2022-03-30 19:37:24 -0300

    up

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,8 +5,8 @@ const ProdutoController = require('../controllers/ProdutoController.js');
 
 routes.get('/', ProdutoController.getAll);
 
-routes.get('/cadastro', ProdutoController.create);
+routes.get('/cadastro', ProdutoController.rota_cad);
 
-routes.get('/detalhes', ProdutoController.show);
+routes.get('/detalhes', ProdutoController.rota_det);
 
 module.exports = routes;
