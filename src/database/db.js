5421895660847cc948c881155b2643bb30eb381e
sirdav1d <ddavid.diniz@gmail.com>
2022-04-01 20:56:59 -0300

    up

--- src/database/db.js
+++ src/database/db.js
@@ -1,17 +1,18 @@
-
+/** @format */
 
 const Sequelize = require('sequelize');
 
 const connection = new Sequelize(
-	process.env.DB_BASE,
-	process.env.DB_USER,
-	process.env.DB_PASS,
+	process.env.DATABASE_URL,
+
 	{
-		host: process.env.DB_HOST,
-		port: 5432,
 		dialect: 'postgres',
-	},
-	
-);
+		dialectOptions:{
+			ssl:{
+				require: true,
+				rejectUnauthorized: false,
+			}
+		}
+	});
 
 module.exports = connection;
