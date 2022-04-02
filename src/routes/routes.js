7e1b6c53a256208ff745329fe6e4d5374d047288
sirdav1d <ddavid.diniz@gmail.com>
2022-04-01 22:31:25 -0300

    up

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,5 +1,5 @@
 const routes = require('express').Router();
-const ProdutoController = require('/controllers/ProdutoController.js');
+const ProdutoController = require('./controllers/ProdutoController.js');
 
 routes.get('/', ProdutoController.getAll);
 
