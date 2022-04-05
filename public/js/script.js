30c0930d009188fdf0755cb9970220f6ce147aef
sirdav1d <ddavid.diniz@gmail.com>
2022-04-05 00:18:36 -0300

    up

--- public/js/script.js
+++ public/js/script.js
@@ -2,18 +2,26 @@ const viewdrop = ()=>{
     const buttons = document.querySelectorAll(".drop-btn");
 
     buttons.forEach((btn)=>{
-        btn.addEventListener("click", (event)=>{
-        const content = event.path[2].children[1]
-        
-        content.classList.toggle("active")
-        if(content.classList.contains("active")){
-            content.style.display = "block";
-        }else{
-            content.style.display = "none";
 
-        }
+            btn.addEventListener("click", (event)=>{
+            const content = event.path[2].children[1]
+            
+            content.classList.toggle("active")
+
+            if(content.classList.contains("active")){
+                content.style.display = "block";
+            }else{
+                content.style.display = "none";
+            }
+
+            content.addEventListener("mouseleave", ()=>{
+                content.classList.remove("active");
+                if(!content.classList.contains("active")){
+                content.style.display = "none";
+                }
+            })
         })
     })
-}
+};
 
 viewdrop();
