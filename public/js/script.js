894b1cce59c7e039fde4684e267dc48745fb5bca
sirdav1d <ddavid.diniz@gmail.com>
2022-04-04 22:47:09 -0300

    up

--- public/js/script.js
+++ public/js/script.js
@@ -1,16 +1,16 @@
 const viewdrop = ()=>{
-    const buttons = document.querySelectorAll('.drop-btn');
+    const buttons = document.querySelectorAll(".drop-btn");
 
     buttons.forEach((btn)=>{
-        btn.addEventListener('click', (event)=>{
+        btn.addEventListener("click", (event)=>{
         const content = event.path[2].children[1];
         
-        content.classlist.toggle('active')
+        content.classlist.toggle("active")
 
-        if(content.classlist.contains('active')){
-            content.style.display = 'block';
+        if(content.classlist.contains("active")){
+            content.style.display = "block";
         }else{
-            content.style.display = 'none';
+            content.style.display = "none";
 
         }
 
