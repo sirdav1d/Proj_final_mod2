23ebc04c15b156f7a787413a545820361ab64077
sirdav1d <ddavid.diniz@gmail.com>
2022-04-06 05:07:20 -0300

    up

--- public/js/script.js
+++ public/js/script.js
@@ -1,39 +1,7 @@
-var header = document.getElementById('header');
-var navigationHeader = document.getElementById('navigation_header');
-var content = document.getElementById('content');
-var showSidebar = false;
-
-function toggleSidebar() {
-	showSidebar = !showSidebar;
-	if (showSidebar) {
-		navigationHeader.style.marginLeft = '-10vw';
-		navigationHeader.style.animationName = 'showSidebar';
-		content.style.filter = 'blur(2px)';
-	} else {
-		navigationHeader.style.marginLeft = '-100vw';
-		navigationHeader.style.animationName = '';
-		content.style.filter = '';
-	}
-}
-
-function closeSidebar() {
-	if (showSidebar) {
-		showSidebar = true;
-		toggleSidebar();
-	}
-}
-
-window.addEventListener('resize', function (event) {
-	if (window.innerWidth > 768 && showSidebar) {
-		showSidebar = true;
-		toggleSidebar();
-	}
-});
-
-var header = document.getElementById('header');
-var navigationHeader = document.getElementById('navigation_header');
-var content = document.getElementById('content');
-var showSidebar = false;
+const header = document.getElementById('header');
+const navigationHeader = document.getElementById('navigation_header');
+const content = document.getElementById('content');
+const showSidebar = false;
 
 function toggleSidebar() {
 	showSidebar = !showSidebar;
@@ -62,38 +30,6 @@ window.addEventListener('resize', function (event) {
 	}
 });
 
-// HEADER
-var headerBar = document.getElementById('header-bar');
-var navigationHeader = document.getElementById('navigation_header');
-var content = document.getElementById('content');
-var showSidebar = false;
-
-function toggleSidebar() {
-	showSidebar = !showSidebar;
-	if (showSidebar) {
-		navigationHeader.style.marginLeft = '-10vw';
-		navigationHeader.style.animationName = 'showSidebar';
-		content.style.filter = 'blur(2px)';
-	} else {
-		navigationHeader.style.marginLeft = '-100vw';
-		navigationHeader.style.animationName = '';
-		content.style.filter = '';
-	}
-}
-
-function closeSidebar() {
-	if (showSidebar) {
-		showSidebar = true;
-		toggleSidebar();
-	}
-}
-
-window.addEventListener('resize', function (event) {
-	if (window.innerWidth > 768 && showSidebar) {
-		showSidebar = true;
-		toggleSidebar();
-	}
-});
 
 toggleSidebar();
 closeSidebar();
