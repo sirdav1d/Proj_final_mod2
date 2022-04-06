0acd5830bda3d49a8a3f62d1d3d3fef295b75968
sirdav1d <ddavid.diniz@gmail.com>
2022-04-06 03:07:07 -0300

    up

--- public/js/script.js
+++ public/js/script.js
@@ -56,5 +56,70 @@ window.addEventListener('resize', function (event) {
 	}
 });
 
+var header = document.getElementById('header');
+var navigationHeader = document.getElementById('navigation_header');
+var content = document.getElementById('content');
+var showSidebar = false;
+
+function toggleSidebar() {
+	showSidebar = !showSidebar;
+	if (showSidebar) {
+		navigationHeader.style.marginLeft = '-10vw';
+		navigationHeader.style.animationName = 'showSidebar';
+		content.style.filter = 'blur(2px)';
+	} else {
+		navigationHeader.style.marginLeft = '-100vw';
+		navigationHeader.style.animationName = '';
+		content.style.filter = '';
+	}
+}
+
+function closeSidebar() {
+	if (showSidebar) {
+		showSidebar = true;
+		toggleSidebar();
+	}
+}
+
+window.addEventListener('resize', function (event) {
+	if (window.innerWidth > 820 && showSidebar) {
+		showSidebar = true;
+		toggleSidebar();
+	}
+});
+
+// HEADER
+var headerBar = document.getElementById('header-bar');
+var navigationHeader = document.getElementById('navigation_header');
+var content = document.getElementById('content');
+var showSidebar = false;
+
+function toggleSidebar() {
+	showSidebar = !showSidebar;
+	if (showSidebar) {
+		navigationHeader.style.marginLeft = '-10vw';
+		navigationHeader.style.animationName = 'showSidebar';
+		content.style.filter = 'blur(2px)';
+	} else {
+		navigationHeader.style.marginLeft = '-100vw';
+		navigationHeader.style.animationName = '';
+		content.style.filter = '';
+	}
+}
+
+function closeSidebar() {
+	if (showSidebar) {
+		showSidebar = true;
+		toggleSidebar();
+	}
+}
+
+window.addEventListener('resize', function (event) {
+	if (window.innerWidth > 768 && showSidebar) {
+		showSidebar = true;
+		toggleSidebar();
+	}
+});
+
 
 viewdrop();
