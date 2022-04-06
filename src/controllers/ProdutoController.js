0993e5659492ac960f307e5d20f1fb36ffe98b81
sirdav1d <ddavid.diniz@gmail.com>
2022-04-06 05:32:51 -0300

    up

--- src/controllers/ProdutoController.js
+++ src/controllers/ProdutoController.js
@@ -1,5 +1,8 @@
 const Produto = require("../models/Produto.js");
 
+let message = '';
+let type = '';
+
 const getAll = async (req, res) => {
 	try {
 		const estoque = await Produto.findAll();
@@ -7,6 +10,8 @@ const getAll = async (req, res) => {
 			estoque,
 			produtoPut: null,
 			produtoDel: null,
+			message,
+			type,
 		});
 	} catch (err) {
 		res.status(500).send({ err: err.message });
@@ -15,7 +20,7 @@ const getAll = async (req, res) => {
 
 const rota_cad = (req, res) => {
 	try {
-		res.render('cadastro');
+		res.render('cadastro', {message, type});
 	} catch (err) {
 		res.status(500).send({ err: err.message });
 	}
@@ -37,9 +42,13 @@ const create = async (req, res) => {
 		const produto = req.body;
 
 		if (!produto) {
+			message = 'Preencha todos os campos para adicionar um sneaker';
+			type = 'danger';
 			return res.redirect('/cadastro');
 		}
 		await Produto.create(produto);
+		message = 'Sneaker Adicionado com sucesso';
+		type = 'success';
 		res.redirect('/');
 	} catch (err) {
 		res.status(500).send({ err: err.message });
@@ -56,12 +65,16 @@ const getById = async (req, res) => {
 				estoque,
 				produtoPut: produto,
 				produtoDel: null,
+				message,
+				type,
 			});
 		} else {
 			res.render('index', {
 				estoque,
 				produtoPut: null,
 				produtoDel: produto,
+				message,
+				type,
 			});
 		}
 	} catch (err) {
@@ -73,6 +86,8 @@ const update = async (req, res) => {
 	try {
 		const produto = req.body;
 		await Produto.update(produto, { where: { id: req.params.id } });
+		message = 'Sneaker Atualizado com sucesso';
+		type = 'success';
 		res.redirect('/');
 	} catch (err) {
 		res.status(500).send({ err: err.message });
@@ -82,11 +97,16 @@ const update = async (req, res) => {
 const remove = async (req, res) => {
 	try {
 		await Produto.destroy({ where: { id: req.params.id } });
+		message = 'Sneaker Apagado com sucesso';
+		type = 'success';
 		res.redirect('/');
 	} catch (err) {
 		res.status(500).send({ err: err.message });
 	}
 };
+
+
+
 
 module.exports = {
 	getAll,
